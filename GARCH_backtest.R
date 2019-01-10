d286b274ec597dac70185f96f71b5d43da6334bc
Alexy Shelest <alexy.shelest@gmail.com>
2019-01-10 08:45:19 +0000

    add back test

---
+++ GARCH_backtest.R
@@ -0,0 +1,81 @@
+library(tseries) 
+library(forecast) 
+library(dplyr)
+library(psych)
+ds = read.csv("VIX.csv") 
+step =  3
+stop = 7
+days = 30
+model = c(1,0,1)
+start <- as.Date("01-01-18",format="%d-%m-%y")
+end   <- as.Date("31-12-18",format="%d-%m-%y")
+hedged_pnl <- function(x) { 
+  if (as.numeric(x[2])>0) { # up
+    if (as.numeric(x[3])>0){ # correct buy signal or stop loss
+      return (ifelse(abs(as.numeric(x[5]))<stop,abs(as.numeric(x[2])),-1*stop))
+    } else {
+      return (ifelse(abs(as.numeric(x[4]))<stop,-1*abs(as.numeric(x[2])),-1*stop))
+    }
+  } else { # down
+    if (as.numeric(x[3])<0){ # correct sell signal or stop loss
+      return (ifelse(abs(as.numeric(x[4]))<stop,abs(as.numeric(x[2])),-1*stop))
+    } else {
+      return (ifelse(abs(as.numeric(x[5]))<stop,-1*abs(as.numeric(x[2])),-1*stop))
+    }
+  }
+}
+pnl <- function(x) {
+  if(as.numeric(x[2])*as.numeric(x[3])>0) 
+    return (abs(as.numeric(x[2])))
+  else 
+    return(-1*abs(as.numeric(x[2])))
+}
+indexesdt = as.matrix(ds[,c("Close")])
+returns = data.frame(DATE= as.Date(ds[-1,]$Date),
+                     ABS_RTRN=diff(ds$Close), PREDICTED=diff(ds$Close),
+                     HIGH_RTRN=as.numeric(ds[-1,]$High-ds[-nrow(ds),]$Close),
+                     LOW_RTRN=as.numeric(ds[-1,]$Low-ds[-nrow(ds),]$Close))
+
+benchmark <- data.frame(returns) %>%
+  filter(DATE < end + step)  %>%
+  filter(DATE >= start) 
+
+for(count in 1:nrow(benchmark)) {
+  if (count == 1 || count %% step == 1) {
+    train = data.frame(returns) %>%
+      filter(DATE < benchmark[count,]$DATE)  # %>%
+    #  filter(DATE > benchmark[count,]$DATE - 355*2) # 2 years is enough
+    test = data.frame(returns) %>%
+      filter(DATE >= benchmark[count,]$DATE) %>%
+      top_n(n=-1*step,wt=DATE)
+    
+    # FIT
+    arma.garch.norm = ugarchspec(mean.model=list(armaOrder=c(1,1,1)), 
+                                 variance.model=list(garchOrder=c(1,1))) 
+    fit.arma.garch.norm = ugarchfit(train$ABS_RTRN, spec=arma.garch.norm, out.sample=9) 
+    forecasts = ugarchforecast(fit.arma.garch.norm, n.ahead=step, n.roll=9)
+
+    for(i in 1:step){
+      if((count+i-1) <= nrow(benchmark))
+        benchmark[count+i-1,]$PREDICTED <- as.vector(forecasts@forecast$seriesFor)
+    }
+  }
+}
+benchmark$PNL <- apply(benchmark, 1, FUN = pnl)
+benchmark$H_PNL <- apply(benchmark, 1, FUN = hedged_pnl)
+
+sum(benchmark %>% filter(PNL != H_PNL) %>% select(PNL)) # where hedge helped
+sum(benchmark %>% filter(PNL != H_PNL) %>% select(H_PNL)) # where hedge helped
+
+hist(benchmark$PNL)
+plot(benchmark$DATE, benchmark$ABS_RTRN)
+lines(benchmark$DATE,benchmark$PREDICTED, col="red", type="p") 
+summary(benchmark$PNL)
+sum(benchmark$PNL)
+sum(benchmark$H_PNL)
+sum(benchmark$ABS_RTRN)
+w<-count(benchmark%>%filter(ABS_RTRN*PREDICTED > 0)) # winner over looser
+l<-count(benchmark%>%filter(ABS_RTRN*PREDICTED <= 0)) # winner over looser
+r<-c(w,l,w/(l+w))
+r
+# ARIMA(1,0,1) can predict VIX movements and 52%-57% times right
